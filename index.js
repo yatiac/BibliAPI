cb1a1d2099335297756415940cdba9bc4a77e678
Rafael E <master.152@gmail.com>
2020-06-04 14:31:08 -0500

    [Heroku testing] Adding proces.env.PORT

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const constants = require("./constants.js");
 const routes = require('./routes.js');
 const swaggerDefinition  = require('./swagger.js');
 
-const port = 3000;
+const port = process.env.PORT || 3000
 
 var options = {
   swaggerDefinition,
